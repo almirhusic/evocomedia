dd613dfd86a4a412b13ec3af7ac5318d9dc5a278
huske_ <almir.adisa.husic@gmail.com>
2019-05-02 12:50:30 +0200

    Form validation

--- src/assets/js/custom/main-contactForm.js
+++ src/assets/js/custom/main-contactForm.js
@@ -10,7 +10,8 @@ $( document ).ready( function ($) {
     dom: function () {
       main.$contactForm = $('form#contact-form');
       main.$submit = $('#submit-form');
-      main.msg = '';
+      main.$msgError = $('.error-message');
+      main.$msgSuccess = $('.success-message');
 
       main.$form = document.forms["contact-form"];
       main.$name = main.$form["name"]; 
@@ -20,25 +21,39 @@ $( document ).ready( function ($) {
     },
 
     events: function () {      
-      var $form = main.$contactForm,
-          url = 'https://script.google.com/macros/s/AKfycbyLFw6qF9-l5Bv9HXuWsvBN4hnQ3fRDGwFriYKWISfbQq72JWg3/exec'
+      var url = 'https://script.google.com/macros/s/AKfycbyLFw6qF9-l5Bv9HXuWsvBN4hnQ3fRDGwFriYKWISfbQq72JWg3/exec'
       
       $(main.$submit).unbind('click.form-submit');
       $(main.$submit).on('click.form-submit', function(e) {
         e.preventDefault();
         var formValid = main.validateForm();
-        
+
         if(formValid) {
+          var $formData = {
+            'name': main.$name.value,
+            'last_name': main.$lastName.value,
+            'email': main.$email.value,
+            'message': main.$message.value
+          }
           var jqxhr = $.ajax({
             url: url,
             method: "GET",
             dataType: "json",
-            data: $form.serialize(),
-            success: main.successMessage()
+            data: $formData,
+            encode: true
+          }).done(function(data) {
+            main.$msgError.removeClass('is-active');
+            main.$msgSuccess.addClass('is-active');            
+            main.$msgSuccess.delay(1000).fadeOut(3000);
+            main.clearFields();
+          })
+          .fail(function(data) {
+            main.$msgError.addClass('is-active');
+            main.$msgSuccess.removeClass('is-active');
           });
         } else {
-          main.failureMessage();
-          main.msg = '';
+          main.$msgError.addClass('is-active');
+          main.$msgSuccess.removeClass('is-active');
         }
       });
       
@@ -59,6 +74,13 @@ $( document ).ready( function ($) {
       });
     },
 
+    clearFields: function () {
+      main.$name.value = '';
+      main.$lastName.value = '';
+      main.$email.value = '';
+      main.$message.value = '';
+    },
+
     validateForm: function () {
       main.$form = document.forms["contact-form"];
       main.$name = main.$form["name"]; 
@@ -71,7 +93,7 @@ $( document ).ready( function ($) {
       var emailValid = main.validateEmail(main.$email);
       var messageValid = main.validateText(main.$message);
 
-      if(main.msg != '') {
+      if(!nameValid || !lastNameValid || !emailValid || !messageValid) {
         return false;
       }
       return true;
@@ -82,7 +104,6 @@ $( document ).ready( function ($) {
 
       if($text.name == "name") {
         if($text.value.length <= 2 || !reg.test($text.value)) {
-          main.msg += "Polje NAME je obavezno i mora sadržavati više od 2 slova";
           main.$name.classList.add('error');
           main.$name.previousElementSibling.classList.add('error');
           return false;
@@ -92,7 +113,6 @@ $( document ).ready( function ($) {
         }
       } else if ($text.name == "last_name") {
         if($text.value.length <= 2 || !reg.test($text.value)) {
-          main.msg += "\nPolje LAST NAME je obavezno i mora sadržavati više od 2 slova";
           main.$lastName.classList.add('error');
           main.$lastName.previousElementSibling.classList.add('error');
           return false;
@@ -102,7 +122,6 @@ $( document ).ready( function ($) {
         }
       } else if ($text.name == "message") {
         if($text.value.length <= 20) {
-          main.msg += "\nPolje MESSAGE je obavezno i mora sadržavati više od 20 karaktera";
           main.$message.classList.add('error');
           main.$message.previousElementSibling.classList.add('error');
           return false;
@@ -116,10 +135,10 @@ $( document ).ready( function ($) {
     },
 
     validateEmail: function (email) {
-      var reg = /\S+@\S+\.\S+/;
+      var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      
       if(!reg.test(email.value) || email.value.length <= 0) {
         main.$email.classList.add('error');
-        main.msg += "\nPolje EMAIL je obavezno ili nije u korektnom formatu.";
         main.$email.previousElementSibling.classList.add('error');
         return false;
       } else {
@@ -127,16 +146,6 @@ $( document ).ready( function ($) {
         main.$email.previousElementSibling.classList.remove('error');
         return true;
       }
-    },
-
-    successMessage: function () {
-      alert("Uspješno poslano");
-      location.reload();
-    },
-
-    failureMessage: function () {
-      //alert(main.msg);
-      return false;
     }
   }
 
